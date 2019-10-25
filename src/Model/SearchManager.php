f0e98176ea7cfe5c58ec7ff9d9f20a27e72bb809
BURSA <atilla.bursa67@gmail.com>
2019-10-25 09:51:06 +0200

    Commit SearchManager

--- src/Model/SearchManager.php
+++ src/Model/SearchManager.php
@@ -14,9 +14,10 @@ class SearchManager extends AbstractManager
     public function search()
     {
         $select = $this->pdo->prepare("SELECT id, type, title, id_category, 
-    DATE_FORMAT(start_hour, '%a %b %H %i') AS 
-    start_hour, DATE_FORMAT(end_hour, '%a %b %H %i') AS end_hour, id_user,
-    DATE_FORMAT(date_publication, '%a %b %H %i') AS date_publication,text_annoucement, nbmin, nbmax FROM ".self::TABLE);
+        DATE_FORMAT(start_hour, '%a %c %b %H %i') 
+        AS start_hour, DATE_FORMAT(end_hour, '%a %c %b %H %i') AS end_hour, id_user,
+        DATE_FORMAT(date_publication, '%a %c %b  %H %i') AS date_publication,text_annoucement, nbmin, nbmax FROM ".
+        self::TABLE);
         // $select->bindValue('post', $name, \PDO::PARAM_STR);
         $select->execute();
         return $select->fetchAll();
