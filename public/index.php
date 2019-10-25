c0db86e5fbee6fbb8553dd7e33c1c71e5a7fda4d
ComteDeX <comtedex@gmail.com>
2019-10-25 10:37:18 +0200

    création de l'US session

--- public/index.php
+++ public/index.php
@@ -1,13 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 11/10/17
- * Time: 14:01
- */
+session_start();
 
+//setcookie('pseudo', 'M@teo21', time() + 365*24*3600, null, null, false, true); // On écrit un cookie
+//setcookie('pays', 'France', time() + 365*24*3600, null, null, false, true); // On écrit un autre cookie...
+//require_once __DIR__ . '/../src/Custom/Auth.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/debug.php';
 require_once __DIR__ . '/../config/db.php';
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../src/routing.php';
+
