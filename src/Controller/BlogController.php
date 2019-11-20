5dbf0c7d284f60afbf65512052e5a8025e02ca2e
ComteDeX <comtedex@gmail.com>
2019-11-20 21:43:16 +0100

    finalisation de la partie blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,20 +7,19 @@ use App\Model\BlogManager;
 
 class BlogController extends AbstractController
 {
-    public function Liste()
+    public function liste()
     {
         $blogManager = new BlogManager();
         $blogListe = $blogManager->selectAll();
         $blogListe = $this->trunc($blogListe); // réduit la taille de l'article à une preview
-        return $this->twig->render('Blog/liste.html.twig',['articles' => $blogListe]);
+        return $this->twig->render('Blog/liste.html.twig', ['articles' => $blogListe]);
     }
 
-    public function Article($id)
+    public function article($id)
     {
         $blogManager = new BlogManager();
         $article = $blogManager->selectOneById($id);
         return $this->twig->render('Blog/article.html.twig', ['article' => $article]);
-
     }
 
     private function trunc(array $tab)
