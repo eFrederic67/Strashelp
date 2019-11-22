d26e01ce2a84262bf2a216097338c12908d4c5bb
Delphinus_dev <delphine.gobbin@gmail.com>
2019-11-22 11:24:48 +0100

    ajouts CSS et liens réparés dans le footer

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -9,16 +9,15 @@ class ContactController extends AbstractController
     /**
      * Display item informations specified by $id
      *
-     * @param int $id
      * @return string
      * @throws \Twig\Error\LoaderError
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
-    public function selectUser(int $id)
+    public function contact()
     {
         $contactManager = new ContactManager();
-        $user = $contactManager->selectUserById($id);
+        $user = $contactManager->selectUserById($_SESSION['Auth']['id']);
 
         return $this->twig->render('Contact/contact.html.twig', ['user' => $user]);
     }
